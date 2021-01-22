d659c4b5f133dead2bb9d39b50b31ac508028f66
Fei Phoon <fei.phoon@zoopla.co.uk>
2021-01-22 00:26:47 +0000

    Add capability to manage groups of stones and liberties

--- goboard_slow.py
+++ goboard_slow.py
@@ -32,3 +32,50 @@ class Move:
     @classmethod
     def resign(cls):
         return Move(is_resign=True)
+
+
+class GoString:
+    """
+    To keep track of connected stones of the same colour, and their liberties.
+    """
+
+    def __init__(self, colour, stones, liberties):
+        self.colour = colour
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        """
+        A liberty is removed when opponents play next to this string.
+        """
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        """
+        A liberty is added when stones adjacent to this string are captured.
+        """
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):
+        """
+        Called when a player connects two of its groups by placing a stone.
+        """
+        assert go_string.colour == self.colour
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.colour,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones,
+        )
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, GoString)
+            and self.colour == other.colour
+            and self.stones == other.stones
+            and self.liberties == other.liberties
+        )
